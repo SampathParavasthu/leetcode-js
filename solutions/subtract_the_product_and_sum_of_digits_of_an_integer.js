7a83537abcaaf01853edad212024c1ff5d2175e5
Sampath Paravasthu <write2sampath@gmail.com>
2023-02-05 00:38:08 +0530

    Create subtract_the_product_and_sum_of_digits_of_an_integer.js

---
+++ solutions/subtract_the_product_and_sum_of_digits_of_an_integer.js
@@ -0,0 +1,12 @@
+#Subtract the Product and Sum of Digits of an Integer
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    let numbers = (n + '').split('');
+    let sum = numbers.reduce((curr,arr) => curr + (+arr),0);
+    let mul = numbers.reduce((curr,arr) => curr * (+arr), 1);
+    return mul - sum;
+};
