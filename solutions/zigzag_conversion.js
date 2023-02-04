91c4a9ded368368f8f624b7d7cfc4992325ca5a2
Sampath Paravasthu <write2sampath@gmail.com>
2023-02-05 00:38:58 +0530

    Create zigzag_conversion.js

---
+++ solutions/zigzag_conversion.js
@@ -0,0 +1,34 @@
+#Zigzag Conversion
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+  let arr = Array.from({ length: numRows}, ()=>'');
+  let row = 0;
+  let down = true;
+  
+  for(let i = 0; i < s.length; i++){
+    if(down){
+      arr[row]+=s[i];
+      ++row;
+    } else {
+      arr[row]+=s[i];
+      --row;
+    }
+    
+    if(row >= numRows){
+      down = false;
+      row-=2;
+      row = row < 0 ? 0 : row;
+    }
+    
+    if(row === 0){
+      down = true;
+    }
+  }
+  
+  return arr.join('');
+};
