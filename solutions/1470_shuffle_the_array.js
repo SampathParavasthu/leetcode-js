bfa0c4cb8c5c39afd46b3327274449d720c2fc04
Sampath Paravasthu <write2sampath@gmail.com>
2023-02-06 16:43:38 +0530

    Create 1470_shuffle_the_array.js

---
+++ solutions/1470_shuffle_the_array.js
@@ -0,0 +1,14 @@
+# 1470 - Shuffle the Array
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let a = [];
+    for(let i = 0; i<n; i++){
+      a.push(nums[i], nums[i + n]);
+    }
+  return a;
+};
